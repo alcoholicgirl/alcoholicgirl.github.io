c55d1bcd89d36ee8601fb98919e2210ddd11c630
alcoholicgirl <2680813175@qq.com>
2025-12-22 20:11:56 +0800

    Github Action

--- astro.config.ts
+++ astro.config.ts
@@ -18,7 +18,9 @@ import netlify from '@astrojs/netlify'
 
 export default defineConfig({
   adapter: netlify(), // Set adapter for deployment, or set `linkCard` to `false` in `src/config.ts`
-  site: themeConfig.site.website,
+  // site: themeConfig.site.website,
+  site: "https://alcoholicgirl.github.io/",
+  // base: "alcoholicgirl.github.io",
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
